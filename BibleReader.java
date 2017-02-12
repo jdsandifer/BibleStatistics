2c8fd8f0e0c4e1a9fb55c5a48d5e8210da005072
J.D. Sandifer <sandifer.jd@gmail.com>
2017-02-11 17:26:04 -0800

    Added separating text into books

--- BibleReader.java
+++ BibleReader.java
@@ -11,29 +11,33 @@ public class BibleReader {
 
   public static void main(String[] args) throws IOException {
     BibleReader text = new BibleReader();
-    
+
     String fullText = text.readTextFile(INPUT_FILE);
-    
-    text.writeTextFile(fullText, OUTPUT_FILE);
+
+    String[] books = fullText.split("\\s+(?=\\s(?:\\d\\s)??[A-z]+\\s+1:1)");
+    //Arrays.sort(books);
+    List<String> outputs = Arrays.asList(books);
+
+    text.writeTextFile(String.join("\n\nNext part:\n", outputs), OUTPUT_FILE);
   }
-      
-  final static String INPUT_FILE = "input.txt";
+
+  final static String INPUT_FILE = "NETBible no markings - last books.txt";
   final static String OUTPUT_FILE = "output.txt";
   final static Charset ENCODING = StandardCharsets.UTF_8;
-  
+
   String readTextFile(String fileName) throws IOException {
     Path path = Paths.get(fileName);
     List<String> lines = Files.readAllLines(path, ENCODING);
-    
+
     return lines.remove(0) + "\n" + lines.remove(0);
   }
-  
+
   void writeTextFile(String textToWrite, String fileName) throws IOException {
     Path path = Paths.get(fileName);
-    
+
     List<String> lines = new ArrayList<String>();
     lines.add(textToWrite);
-    
+
     Files.write(path, lines, ENCODING);
   }
 }
