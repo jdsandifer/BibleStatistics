016dde87982ce25f1fb34a7769fe86429174cd4c
J.D. Sandifer <sandifer.jd@gmail.com>
2017-02-18 01:00:43 -0800

    Add encoding testing functionality (first try)

--- src/EncodingReader.java
+++ src/EncodingReader.java
@@ -6,22 +6,25 @@ import java.nio.charset.StandardCharsets;
 public class EncodingReader {
   final static String TEST_FILE = "..\\input\\test-file.txt";
   final static Charset[] ENCODINGS = {StandardCharsets.UTF_8,
-                                       StandardCharsets.US_ASCII,
-                                       StandardCharsets.ISO_8859_1,
-                                       StandardCharsets.UTF_16,
-                                       StandardCharsets.UTF_16BE,
-                                       StandardCharsets.UTF_16LE}
+                                      StandardCharsets.US_ASCII,
+                                      StandardCharsets.ISO_8859_1,
+                                      StandardCharsets.UTF_16,
+                                      StandardCharsets.UTF_16BE,
+                                      StandardCharsets.UTF_16LE};
 
   public static void main(String[] args) {
-    BibleReader text = new BibleReader;
+    BibleReader text = new BibleReader();
 
     // Try each encoding and list encodings that work.
     for (int i = 0; i < ENCODINGS.length; i++) {
       try {
-        text.readTextFile(TEST_FILE, ENCODINGS[i]);
+        String lines = text.readTextFile(TEST_FILE, ENCODINGS[i]);
         System.out.println("This encoding worked: "
-                           + ENCODINGS[i].toString
+                           + ENCODINGS[i].toString()
                            + ".");
+        System.out.println(lines);
+      } catch(IOException e) {
+        System.out.println("IO Error: " + e.toString());
       }
     }
   }
