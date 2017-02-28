3dca9dd5cb4bb2dc52398fc049769f7b5955bcb9
J.D. Sandifer <sandifer.jd@gmail.com>
2017-02-28 00:30:50 -0800

    Update FileFormatter.java

--- src/FileFormatterTest.java
+++ src/FileFormatterTest.java
@@ -6,12 +6,16 @@ public class FileFormatterTest {
   static List<Book> books;
   @Before
   public void setUp() {
-    books = { new Book("Joe\r\n1:1 Hi there. 1:2 Bye!"),
-              new Book("Bob\r\n1:1 Yo! 1:2 What's up?"),
-              new Book("Frank\r\n1:1 What's happening? \r\n1:2 Nothing much. You?\r\n2:1 Life is happening...") }
+    books = Arrays.asList("Joe\r\n1:1 Hi there. 1:2 Bye!",
+                          "Bob\r\n1:1 Yo! 1:2 What's up?",
+                          "Frank\r\n1:1 What's happening? \r\n1:2 Nothing much. You?\r\n2:1 Life is happening...");
   }
 
+  @Test
   public void formatBookTextReturnsString() {
+    String expectation = "Joe -> 3 words\r\n\r\n1:1 Hi there. 1:2 Bye!\r\n\r\n\r\nBob -> 3 words\r\n\r\n1:1 Yo! 1:2 What's up?\r\n\r\n\r\nFrank -> 3 words\r\n\r\n1:1 What's happening? \r\n1:2 Nothing much. You?\r\n2:1 Life is happening...\r\n\r\n\r\n";
+    String result = FileFormatter.formatBookText(books);
 
+    assertEquals("FileFormatter should return formatted string.", expectation, result);
   }
 }
