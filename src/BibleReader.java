11e953d90929b0ecc922462ce804d2355a13fa4e
J.D. Sandifer <sandifer.jd@gmail.com>
2017-03-02 11:32:16 -0800

    Remove refactored BibleReader methods

--- src/BibleReader.java
+++ src/BibleReader.java
@@ -13,28 +13,6 @@ public class BibleReader {
   final static Charset INPUT_ENCODING = StandardCharsets.US_ASCII;
   final static Charset OUTPUT_ENCODING = StandardCharsets.UTF_8;
 
-  public static List<Chapter> bookToChapters(Book book) {
-    String bookTitle = book.getTitle();
-    String[] chapTexts = book.getText()
-                             .trim()
-                             .split("\\s+(?=\\d{1,3}:1\\s)");
-    List<Chapter> chapters = Arrays.stream(chapTexts)
-                                   .map(c->new Chapter(bookTitle, c))
-                                   .collect(Collectors.toList());
-    return chapters;
-  }
-
-  public static List<Verse> chapterToVerses(Chapter chapter) {
-    String chapterInfo = chapter.getBookAndNumber();
-    String[] verseTexts = chapter.getText()
-                                 .trim()
-                                 .split("\\s+(?=\\d{1,3}:\\d{1,3})");
-    List<Verse> verses = Arrays.stream(verseTexts)
-                                   .map(v->new Verse(chapterInfo, v))
-                                   .collect(Collectors.toList());
-    return verses;
-  }
-
   public String readTextFile(String fileName) throws IOException {
     Path path = Paths.get(fileName);
     List<String> lines = Files.readAllLines(path, INPUT_ENCODING);
