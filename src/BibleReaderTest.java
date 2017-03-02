c4c44516908b84109b911f310057b6a18dd0bc98
J.D. Sandifer <sandifer.jd@gmail.com>
2017-03-02 11:32:30 -0800

    Create BibleReaderTest

---
+++ src/BibleReaderTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+public class BibleReaderTest {
+  @Test
+  public void readTextFileReturnsText() {
+    fail("No test written yet...");
+  }
+
+  @Test
+  public void writeTextFileCreatesFile() {
+    fail("No test written yet...");
+  }
+}
