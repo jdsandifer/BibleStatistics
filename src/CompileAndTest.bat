049f4d4d5760c03a40a3e08ade21f8c80d9b4ebe
J.D. Sandifer <sandifer.jd@gmail.com>
2017-03-08 21:10:40 -0800

    Update batch file to be less verbose

--- src/CompileAndTest.bat
+++ src/CompileAndTest.bat
@@ -2,14 +2,6 @@ REM Drag a .java file onto this batch file to
 REM compile it and it's test file, and run the test.
 
 @ECHO OFF
-ECHO.
-ECHO CompileAndTest.bat, v1.0.0 for Windows 7
-ECHO Compile the provided file, it's test file, and run test.
-ECHO.
-ECHO Usage: Drag file onto this batch to run.
-ECHO.
-ECHO Written by J.D. Sandifer, http://github.com/jdsandifer
-ECHO.
 
 SETLOCAL
 
@@ -19,11 +11,6 @@ SET "fileName=%className%.java"
 SET "testFileName=%className%Test.java"
 SET "testName=%className%Test"
 
-REM Check names...
-SET fileName
-SET testFileName
-SET testName
-
 @ECHO ON
 javac %fileName%
 javac -cp .;junit-4.12.jar %testFileName%
@@ -32,5 +19,5 @@ java -cp .;junit-4.12.jar;hamcrest-core-1.3.jar org.junit.runner.JUnitCore %test
 
 ENDLOCAL
 
-ECHO.
+REM Keep cmd window open... (is there a better way?)
 PAUSE
