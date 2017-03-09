690f760a093e6159d1c8d25e6b63d24741a20c39
J.D. Sandifer <sandifer.jd@gmail.com>
2017-03-08 21:46:39 -0800

    Add methods removed from BibleReader

--- src/BibleParser.java
+++ src/BibleParser.java
@@ -16,14 +16,36 @@ public class BibleParser {
   public static List<Chapter> parseChapters(List<Book> books) {
     // Use book data list to create chapter data List
     return books.stream()
-                .flatMap(b->BibleReader.bookToChapters(b).stream())
+                .flatMap(b->bookToChapters(b).stream())
                 .collect(Collectors.toList());
   }
 
   public static List<Verse> parseVerses(List<Chapter> chapters) {
     // Use chapter data list to create verse data list
     return chapters.stream()
-                   .flatMap(c->BibleReader.chapterToVerses(c).stream())
+                   .flatMap(c->chapterToVerses(c).stream())
                    .collect(Collectors.toList());
+  }
+
+  static List<Chapter> bookToChapters(Book book) {
+    String bookTitle = book.getTitle();
+    String[] chapTexts = book.getText()
+                             .trim()
+                             .split("\\s+(?=\\d{1,3}:1\\s)");
+    List<Chapter> chapters = Arrays.stream(chapTexts)
+                                   .map(c->new Chapter(bookTitle, c))
+                                   .collect(Collectors.toList());
+    return chapters;
+  }
+
+  static List<Verse> chapterToVerses(Chapter chapter) {
+    String chapterInfo = chapter.getBookAndNumber();
+    String[] verseTexts = chapter.getText()
+                                 .trim()
+                                 .split("\\s+(?=\\d{1,3}:\\d{1,3})");
+    List<Verse> verses = Arrays.stream(verseTexts)
+                                   .map(v->new Verse(chapterInfo, v))
+                                   .collect(Collectors.toList());
+    return verses;
   }
 }
