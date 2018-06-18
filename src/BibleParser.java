26521dd042bd22c0c1ceb26d33104344b4f80ba6
J.D. Sandifer <sandifer.jd@gmail.com>
2018-06-18 12:04:46 -0700

    Tweaked comments for clarity

--- src/BibleParser.java
+++ src/BibleParser.java
@@ -3,26 +3,32 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class BibleParser {
+  
+  // Splits up text into books and create book data list
   public static List<Book> parseBooks(String fullText) {
-    // Split up text into books and create book data list
+    // Regex will split up book text by looking for the 1:1
+    // chaper/verse marking at the start of each boock.
     String regexToSplitTextByBooks =
         "\\s+(?=\\s(?:\\d\\s)??(?:[A-z]+\\s)+\\s*1:1)";
     String[] bookSplits = fullText.split(regexToSplitTextByBooks);
+    
     List<String> bookTexts = Arrays.asList(bookSplits);
+    
+    // The constructor of Book will take the text as split above
     return bookTexts.stream()
                     .map(Book::new)
                     .collect(Collectors.toList());
   }
 
+  // Uses book data list to create chapter data List
   public static List<Chapter> parseChapters(List<Book> books) {
-    // Use book data list to create chapter data List
     return books.stream()
                 .flatMap(eachBook->bookToChapters(eachBook).stream())
                 .collect(Collectors.toList());
   }
 
+  // Uses chapter data list to create verse data list
   public static List<Verse> parseVerses(List<Chapter> chapters) {
-    // Use chapter data list to create verse data list
     return chapters.stream()
                    .flatMap(eachChapter->chapterToVerses(eachChapter).stream())
                    .collect(Collectors.toList());
@@ -30,10 +36,14 @@ public class BibleParser {
 
   static List<Chapter> bookToChapters(Book book) {
     String bookTitle = book.title();
+    
+    // This regex looks for chapter/verse markers to find
+    // the start of each chapter (e.g. 110:1).
     String regexToSplitTextByChapters = "\\s+(?=\\d{1,3}:1\\s)";
     String[] chapTexts = book.text()
                              .trim()
                              .split(regexToSplitTextByChapters);
+    
     List<Chapter> chapters = Arrays.stream(chapTexts)
                                    .map(c->new Chapter(bookTitle, c))
                                    .collect(Collectors.toList());
@@ -42,10 +52,14 @@ public class BibleParser {
 
   static List<Verse> chapterToVerses(Chapter chapter) {
     String chapterInfo = chapter.bookAndNumber();
+    
+    // This regex looks for verses by finding the references
+    // at the start of each verse (e.g. 99:5).
     String regexToSplitTextByVerses = "\\s+(?=\\d{1,3}:\\d{1,3})";
     String[] verseTexts = chapter.text()
                                  .trim()
                                  .split(regexToSplitTextByVerses);
+    
     List<Verse> verses = Arrays.stream(verseTexts)
                                    .map(eachVerse->new Verse(chapterInfo, eachVerse))
                                    .collect(Collectors.toList());
